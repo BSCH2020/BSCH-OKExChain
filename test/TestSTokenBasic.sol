f7f1890432d2c5dc42ce83979180e8b421cb62ef
BSCH2020 <contact@bsch.finance>
2019-04-22 18:56:19 +0800

    Simplification Superhero scans

---
+++ test/TestSTokenBasic.sol
@@ -0,0 +1,11 @@
+pragma solidity>=0.6.9;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Bitcoin Standard Circulation Hashrate TokenToken.sol";
+import "../contracts/BSCH.sol";
+
+contract TestSToken{
+
+    
+}
